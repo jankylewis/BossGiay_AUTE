eadbf12b0cd9ea320a29aa343fcf1c9c776aa0b1
kop <kop02092k@gmail.com>
2021-12-01 22:16:12 +0700

    Update keyword selecting randomly for search func

--- src/test/java/com/bossgiay/testScripts/TSs_SearchSneakers.java
+++ src/test/java/com/bossgiay/testScripts/TSs_SearchSneakers.java
@@ -19,6 +19,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 
 @Listeners({TestListeners.class})
@@ -32,17 +33,17 @@ public class TSs_SearchSneakers extends TestAbstractClass {
     private boolean checkPointIfFailed;
     private boolean checkPointIfPassed;
     private boolean checkPointIfSkipped;
-    private final String searchKey= "JORDAN";
+    private String searchKey;
     private final String pageTitle= "TÌM KIẾM";
     private final String headingText= "Kết quả tìm kiếm cho"+ "\""+ searchKey+ "\".";
 
-    private final By     LBL_PAGE_TITLE_LOCATOR= By.xpath("//div[@class=\"heading-page\"]//child::h1");
-    private final By     LBL_HEADING_TEXT_LOCATOR= By.xpath("//p[@class=\"subtext-result\"]");
-    private final By     LBL_SNEAKER_NAME_CHILD_LOCATOR= By.xpath("//h3[@class=\"pro-name\"]//a");
-    private final By     LBL_SNEAKER_NAME_PARENT_LOCATOR= By.xpath("//div[contains(@class,\"search-list\")]");
+    private final By LBL_PAGE_TITLE_LOCATOR= By.xpath("//div[@class=\"heading-page\"]//child::h1");
+    private final By LBL_HEADING_TEXT_LOCATOR= By.xpath("//p[@class=\"subtext-result\"]");
+    private final By LBL_SNEAKER_NAME_CHILD_LOCATOR= By.xpath("//h3[@class=\"pro-name\"]//a");
+    private final By LBL_SNEAKER_NAME_PARENT_LOCATOR= By.xpath("//div[contains(@class,\"search-list\")]");
 
-
-    private       List<String> sneakersArray= new ArrayList<String>();
+    private List<String> sneakersArray= new ArrayList<String>();
+    private String searchSneakerOnePageKey[];
 
     @BeforeMethod(alwaysRun = true,
                     enabled = true,
@@ -64,6 +65,63 @@ public class TSs_SearchSneakers extends TestAbstractClass {
                     description = "returned result is less than two page")
     public void searchSneakerTest01() throws TimeoutException, AWTException {
 
+//        select randomly a keyword
+        this.searchSneakerOnePageKey= new String[] {
+        "JORDAN",
+                "LOW",
+                "MID",
+                "STAN",
+                "NIKE",
+                "ADIDAS",
+                "SUPER",
+                "HOLOGRAM",
+                "DOMBA",
+                "WHITE",
+                "BLACK",
+                "RAINBOWN",
+                "SMITH",
+                "ALL",
+                "MAX",
+                "VICTOR",
+                "MLB",
+                "SHADOW",
+                "DOGGER",
+                "SLIDE",
+                "GOLD",
+                "HIGH",
+                "ICE",
+                "META",
+                "LASER",
+                "RED",
+                "SIMPSONS",
+                "ADLV",
+                "BIG",
+                "X",
+                "V",
+                "PUMA",
+                "JAN",
+                "K",
+                "YANKEE",
+                "MONOGRAM",
+                "FORCE",
+                "FRUIT",
+                "ROSE",
+                "CARTOON",
+                "POUCH",
+                "WORLDWIDE",
+                "ASUNA",
+                "JACQUARD",
+                "F",
+                "MON",
+                "HOODY",
+                "LIFE",
+                "VOLT",
+                "METALLIC",
+                "POINT",
+                "SP",
+                "2"
+        };
+        this.searchKey= searchSneakerOnePageKey[new Random().nextInt(searchSneakerOnePageKey.length)];
 //        project to SearchProductPage to use method
         SearchProductPage searchPage= new SearchProductPage(driver);
         searchPage.clickOnSearchButton();
@@ -121,7 +179,8 @@ public class TSs_SearchSneakers extends TestAbstractClass {
                 this.checkPointIfSkipped=false;
                 if (checkPointIfPassed && !checkPointIfFailed && !checkPointIfSkipped) {
                     Assert.assertFalse(false);
-                    log.info("//------**ASSERTED SNEAKER NUMBER "+ index+ "!-------//"+ "\n");//                       add sneaker name to the array
+                    log.info("//------**ASSERTED SNEAKER NUMBER "+ index+ "!-------//"+ "");
+//                    add sneaker name to the array
                     sneakersArray.add(sneakerName.toUpperCase(Locale.ROOT));
 //                   print out the has-been-added array
                     System.out.println("Sneaker number "+ index+ " is inserted " +
